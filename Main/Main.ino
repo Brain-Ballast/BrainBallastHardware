1304bf343302117c500d307ab3bbf12ea790ab40
Hayden-Threlfall <89491692+Hayden-Threlfall@users.noreply.github.com>
2025-09-22 13:01:54 -0500

    Revert "testing new stuff"
    This reverts commit b7d2d60cc6ff51d35443eaf9fd27347cab408725.

--- Main/Main.ino
+++ Main/Main.ino
@@ -1,5 +1,6 @@
 #define SERIAL_BAUD 115200
 
+// Global variable definitions (only define once here)
 char serialBuffer[128];
 char outputBuffer[8192];
 char storageBuffer[8192]; 
@@ -7,12 +8,6 @@ char storageBuffer[8192];
 unsigned long lastSensorReading = 0;
 unsigned long lastBTTransmit = 0;
 unsigned long lastStorageWrite = 0;
-unsigned long lastBTReconnect = 0;
-unsigned long lastHzReport = 0;
-
-
-unsigned long loopCount = 0;
-unsigned long lastHzTime = 0;
 
 void setup() {
     Serial.begin(SERIAL_BAUD);
@@ -21,23 +16,18 @@ void setup() {
     outputBuffer[0] = '\0';
     storageBuffer[0] = '\0';
     
-    // Initialize Hz tracking
-    lastHzTime = millis();
-    
     pressureSetup();
     adxlSetup();
     storageSetup();
     connectionSetup();
     
-    // Add CSV header with timestamp
-    sprintf(serialBuffer, "pres,temp,x,y,z,timestamp\n");
+    // Add CSV header
+    sprintf(serialBuffer, "pres,temp,x,y,z\n");
     Serial.print("CSV Header: ");
     Serial.print(serialBuffer);
 }
 
 void loop() {
-    loopCount++;
-    
     if (checkTimer(lastSensorReading, 100)) {
         readSensorsToCSV();
     }
@@ -48,22 +38,16 @@ void loop() {
             Serial.print(serialBuffer);
             Serial.print(outputBuffer);
             
-            if (btSendData(outputBuffer)) {
-                // Successfully sent
-            } else {
-                sprintf(serialBuffer, "BT not connected, data cached\n");
-                Serial.print(serialBuffer);
-            }
-                        
+        if (btSendData(outputBuffer)) {
+            // Successfully sent
+        } else {
+            sprintf(serialBuffer, "BT not connected, data cached\n");
+            Serial.print(serialBuffer);
+        }
+                    
             // Clear the BT buffer
             outputBuffer[0] = '\0';
         }
-    }
-
-    if (!btIsConnected() && checkTimer(lastBTReconnect, 120000)) { // 2 minutes = 120000ms
-        sprintf(serialBuffer, "Attempting BT reconnection...\n");
-        Serial.print(serialBuffer);
-        btReconnect();
     }
 
     if (checkTimer(lastStorageWrite, 3000)) {
@@ -73,10 +57,6 @@ void loop() {
             storageWrite(storageBuffer);
             storageBuffer[0] = '\0';
         }
-    }
-
-    if (checkTimer(lastHzReport, 5000)) {
-        calculateAndReportHz();
     }
 }
 
@@ -89,35 +69,14 @@ bool checkTimer(unsigned long &lastTrigger, unsigned long interval) {
     return false;
 }
 
-void calculateAndReportHz() {
-    unsigned long currentTime = millis();
-    unsigned long timeDiff = currentTime - lastHzTime;
-    
-    if (timeDiff > 0) {
-        float actualHz = (float)loopCount * 1000.0 / (float)timeDiff;
-        sprintf(serialBuffer, "Runtime Hz: %.2f (loops: %lu, time: %lu ms)\n", 
-                actualHz, loopCount, timeDiff);
-        Serial.print(serialBuffer);
-    }
-    
-    // Reset counters
-    loopCount = 0;
-    lastHzTime = currentTime;
-}
-
 void readSensorsToCSV() {
-    char csvLine[256];
+    char csvLine[128];
     float pressure, temperature;
     pressureRead(&pressure, &temperature);
     float x_g, y_g, z_g;
     adxlRead(&x_g, &y_g, &z_g);
-    
-    // Get current timestamp in milliseconds
-    unsigned long timestamp = millis();
-    
-    sprintf(csvLine, "%.2f,%.2f,%.2f,%.2f,%.2f,%lu\n", 
-            pressure, temperature, x_g, y_g, z_g, timestamp);
-            
+    sprintf(csvLine, "%.2f,%.2f,%.2f,%.2f,%.2f\n", 
+            pressure, temperature, x_g, y_g, z_g);
     if (strlen(outputBuffer) + strlen(csvLine) < sizeof(outputBuffer) - 1) {
         strcat(outputBuffer, csvLine);
     } else {
